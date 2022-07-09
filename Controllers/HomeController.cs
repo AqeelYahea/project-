65d46da4983ff901d5ae2a95e1b24736fbc26421
MC <MC@DESKTOP-7V46L6C>
2022-07-09 22:03:47 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project_.Models;
+using System.Diagnostics;
+
+namespace project_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            return View();
+        }
+
+        public IActionResult Jobs()
+        {
+            return View();
+        }
+
+        public IActionResult Facilite()
+        {
+            return View();
+        }
+
+
+        public IActionResult Mission()
+        {
+            return View();
+        }
+
+        public IActionResult Foundation()
+        {
+            return View();
+        }
+
+        public IActionResult Membership()
+        {
+            return View();
+
+        }
+
+        public IActionResult BoardDirect()
+        {
+
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
